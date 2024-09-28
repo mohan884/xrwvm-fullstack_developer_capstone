435bcb0792c9f6bfb2e3a5f29d3a322f108bbb75
mohan884 <ram1234569879@outlook.com>
2024-09-28 21:57:59 +0530

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -3,7 +3,12 @@
 
 
 # Register your models here.
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+# Registering models with their respective admins
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 # CarModelInline class
 
 # CarModelAdmin class
